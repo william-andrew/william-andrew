e405cca33c8e67918d3094e93e3995c5a2870297
Andrew Xu <xuwd@hotmail.com>
2012-01-22 17:50:58 -0800

    add ITransactionStorage and Resources

---
+++ MyRM/ITransactionStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using TP;
+namespace MyRM
+{
+    interface ITransactionStorage
+    {
+        Dictionary<Customer, HashSet<RID>> GetReservations();
+        void SetReservations(Dictionary<Customer, HashSet<RID>> reservations);
+        Dictionary<RID, Resource> GetResources();
+        void SetResources(Dictionary<RID, Resource> resources);
+    }
+}
