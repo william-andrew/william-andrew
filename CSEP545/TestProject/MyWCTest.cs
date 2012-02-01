52b07e905067844f6075cf58de5126335a50d69b
unknown <weidxu@ANDREWXU-MSFT.redmond.corp.microsoft.com>
2012-02-01 04:58:59 -0800

    added a wc test case for addseats

---
+++ CSEP545/TestProject/MyWCTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Transaction = TP.Transaction;
+
+namespace TestProject
+{
+    [TestClass()]
+    public class MyWCTest
+    {
+        /// <summary>
+        ///A test for AddSeats
+        ///</summary>
+        [TestMethod()]
+        public void AddSeatsTest()
+        {
+            var wc = new MyWC.MyWC();
+            var tm = new MyTM.MyTM();
+            var rm = new MyRM.MyRM();
+            rm.SetName("flight");
+            tm.Register(rm);
+            MyWC.MyWC.Flights = tm.GetResourceMananger("flight");
+ 
+            var context = new Transaction();
+            var success = wc.AddSeats(context, "FL", 100, 550);
+
+            tm.Commit(context);
+            
+
+        }
+    }
+}
